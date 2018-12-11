3c580e81530ba254f1d3dadab96c9bc1c09fb7b5
Tim James <tjames@tjames.com>
2018-12-11 15:21:41 -0800

    Adds the gulp tasks.

--- README.md
+++ README.md
@@ -1,3 +1,50 @@
-# gulp-rev
+# `gulp-static-web`
 
-`gulp` tasks to cache-bust static assets via rev filename thumbprints via `gulp-rev` and `gulp-rev-replace`.
+Opinionated `gulp` tasks for development and build of a static web site, as for a Single Page App.
+- **TODO:** Builds JS via Browserify.
+- Compiles CSS via `postcss`, using injected plugins, generating sourcemaps.
+- Cache-busts static assets via rev filename thumbprints via `gulp-rev` and `gulp-rev-replace`.
+
+Supplies `build`, `static`, `css` tasks.
+
+`build` performs thumbprinting and calls a `default` task (to be supplied by you, the user), which should call `static` and `css` tasks.
+
+## Directories
+
+**TODO**
+
+## Usage
+
+Your `gulpfile.js` might look like:
+
+```javascript
+const gulp = require('gulp')
+const sequence = require('gulp-sequence').use(gulp)
+const gulpStaticWeb = require('gulp-static-web')
+
+gulpStaticWeb(gulp, {
+  postcss: [
+    require('precss'),
+    require('postcss-nested-vars'),
+    // ...
+  ],
+})
+
+gulp.task('default', sequence('static', 'css'))
+```
+
+During development, you might use:
+
+```bash
+nodemon \
+ -e js,yml,yaml,html,css \
+ -w style/ \
+ -w gulpfile.js \
+ -x gulp css &
+
+nodemon \
+ -e js,yml,yaml,html,css \
+ -w static/ \
+ -w gulpfile.js \
+ -x gulp static &
+```
---
+++ index.js
@@ -0,0 +1,52 @@
+
+module.exports = (gulp, opts) => {
+  const sequence = require('gulp-sequence').use(gulp)
+  const rev = require('gulp-rev')
+  const revReplace = require('gulp-rev-replace')
+
+  gulp.task('build', sequence('rimraf', 'default', 'rev-replace'))
+
+  gulp.task('css', () => {
+    const postcss    = require('gulp-postcss')
+    const sourcemaps = require('gulp-sourcemaps')
+
+    return gulp.src('style/**/*.css')
+      .pipe( sourcemaps.init() )
+      .pipe( postcss(opts.postcss || []) )
+      .pipe( sourcemaps.write('.') )
+      .pipe( gulp.dest('out/') )
+  })
+
+  gulp.task('static', () =>
+    gulp.src('./static/**')
+      .pipe(gulp.dest('out'))
+  )
+
+
+  // "Private" tasks, implementation details
+
+  gulp.task('revision', () =>
+    gulp.src(['out/**', '!**/*.html', '!**/favicon.ico'])
+      .pipe(rev())
+      .pipe(gulp.dest('build'))
+      .pipe(rev.manifest())
+      .pipe(gulp.dest('build'))
+  )
+
+  gulp.task('no-revision', () =>
+    gulp.src(['out/**/*.html', 'out/**/favicon.ico'])
+      .pipe(gulp.dest('build'))
+  )
+
+  gulp.task('rev-replace', ['revision', 'no-revision'], () =>
+    gulp.src(['build/**/*.html', 'build/**/*.css', 'build/**/*.js'])
+      .pipe(revReplace({manifest: gulp.src('./build/rev-manifest.json')}))
+      .pipe(gulp.dest('build'))
+  )
+
+  gulp.task('rimraf', (cb) => {
+    const rimraf = require('rimraf')
+    rimraf('./out', () => rimraf('./build', cb))
+  })
+
+}
